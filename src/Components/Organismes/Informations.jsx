e7e9d1ab817325ca0f400d3b8f590738c4876955
bennet489 <bennetasare@outlook.com>
2024-05-28 02:00:39 +0300

    Implement the Information card

---
+++ src/Components/Organismes/Informations.jsx
@@ -0,0 +1,17 @@
+import InfoGender from "../Molecules/Info things/InfoGender";
+import InfoAge from "../Molecules/Info things/InfoAge";
+import InfoCountry from "../Molecules/Info things/InfoCountry";
+
+function Information() {
+  return (
+    <>
+      <div>
+        <InfoGender />
+        <InfoAge />
+        <InfoCountry />
+      </div>
+    </>
+  );
+}
+export default Information;
+
