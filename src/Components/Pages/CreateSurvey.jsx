fd3603cc72f863e28c362511380df0498ac67be5
bennet489 <bennetasare@outlook.com>
2024-05-28 02:10:01 +0300

    Design QuestionsList card

---
+++ src/Components/Pages/CreateSurvey.jsx
@@ -0,0 +1,17 @@
+import SurveyInformations from "../Molecules/SurveyInformations";
+import QuestionLists from "../Organisms/QuestionLists";
+
+const CreateSurvey = () => {
+ 
+  return (
+    <div>
+      <h1 className="text-3xl text-center pt-[150px] text-[#042B29] pb-14 font-bold">Create Survey</h1>
+      <div className=" mx-auto max-w-screen-lg bg-[#5EB7BF]/20 rounded-lg px-[100px] shadow-lg pb-[30px]">
+        <SurveyInformations/>
+        <QuestionLists/>
+              </div>
+        </div>
+  );
+};
+
+export default CreateSurvey;
