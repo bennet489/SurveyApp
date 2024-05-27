67e63919d4222d79541957088863c5ca23c04e78
Raniaelmorabet <raniaelmorabet9@gmail.com>
2024-05-27 23:02:22 +0100

    Setuping Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
-import {Router} from "react-router-dom";
-import React from "react";
-import "./Style/index.css"
-import NavBar from "./Components/Organismes/NavBar.jsx";
-export default function App() {
-    return(
+import "./Style/index.css";
+import NavBar from "./Components/Organisms/NavBar.jsx";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import CreateSurvey from "./Components/Pages/CreateSurvey.jsx";
+import { Footer } from "./Components/Organisms/Footer.jsx";
+import { Home } from "./Components/Pages/Home.jsx";
+import Response from "./Components/Pages/Response.jsx";
+import { Contact } from "./Components/Pages/Contact.jsx";
+import ReportPage from "./Components/Pages/ReportPage";
+import ProviderContext from "./Context/ProviderContext.jsx";
+import { AboutUs } from "./Components/Pages/AboutUs";
+
+function App() {
+    return (
         <Router>
             <div>
-                <NavBar/>
+                <NavBar />
             </div>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <ProviderContext>
+                            <Home />
+                        </ProviderContext>
+                    }
+                ></Route>
+                <Route
+                    path="/Contact"
+                    element={
+                        <ProviderContext>
+                            <Contact />
+                        </ProviderContext>
+                    }
+                ></Route>
+                <Route
+                    path="/Response/:id"
+                    element={
+                        <ProviderContext>
+                            <Response />
+                        </ProviderContext>
+                    }
+                ></Route>
+
+                <Route
+                    path="/createSurvey"
+                    element={
+                        <ProviderContext>
+                            <CreateSurvey />
+                        </ProviderContext>
+                    }
+                ></Route>
+                <Route
+                    path="/reportPage/:id"
+                    element={
+                        <ProviderContext>
+                            <ReportPage />
+                        </ProviderContext>
+                    }
+                ></Route>
+                <Route path="/ReportPage" element={<ReportPage />}></Route>
+                <Route path="/AboutUs" element={<AboutUs />} />
+            </Routes>
+            <Footer />
         </Router>
     );
 }
+
+export default App;
