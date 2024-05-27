1d7fbf4db41905d233205d3d3147935afbe1d78e
bennet489 <bennetasare@outlook.com>
2024-05-28 02:08:39 +0300

    Design the Contact card

---
+++ src/Components/Pages/Contact.jsx
@@ -0,0 +1,11 @@
+import { ContactCard } from "../Organisms/ContactCard.jsx";
+import React, {useState} from "react";
+
+export function Contact() {
+    return (
+        <div id='Card-Container'>
+            <ContactCard/>
+        </div>
+    );
+}
+
